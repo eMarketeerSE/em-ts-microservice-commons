2304b27f83ffc871baad28593db10c2e9afce838
arsenyyankovski <arseny.yankovsky@emarketeer.com>
2024-10-22 14:33:45 +0200

    feat(esbuild): patch mysql2 module with recap.dev

--- src/esbuild-plugins.js
+++ src/esbuild-plugins.js
@@ -1,3 +1,31 @@
 const esbuildPluginTsc = require('@emarketeer/esbuild-plugin-tsc')
+const fs = require('fs')
+const { dirname } = require('path')
 
-module.exports = [esbuildPluginTsc()]
+const recapDevAutoWrapper = {
+  name: 'recap-dev-auto-wrapper',
+  setup(build) {
+    build.onLoad({ filter: /mysql2\/index.js/ }, async ({ path }) => {
+      const originalSource = fs.readFileSync(path, 'utf8')
+
+      return ({
+        resolveDir: dirname(path),
+        contents: `
+          (function() {
+    ${originalSource}
+  })(...arguments);
+  {
+  mod = module.exports;
+  const { mysqlQueryWrapper } = require('@recap.dev')
+
+  mod.Connection.prototype.query = mysqlQueryWrapper(mod.Connection.prototype.query);
+  mod.Connection.prototype.execute = mysqlQueryWrapper(mod.Connection.prototype.execute);
+  module.exports = mod;
+  }
+  `,
+      })
+    })
+  },
+}
+
+module.exports = [recapDevAutoWrapper, esbuildPluginTsc()]
