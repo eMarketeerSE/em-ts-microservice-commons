ada06004d59c41a9e54f86ee4ca9822e3421e7e8
arsenyyankovski <arseny.yankovsky@emarketeer.com>
2024-10-22 13:07:21 +0200

    feat(esbuild): patch mysql2 module with recap.dev

--- src/esbuild-plugins.js
+++ src/esbuild-plugins.js
@@ -1,23 +1,28 @@
 const esbuildPluginTsc = require('@emarketeer/esbuild-plugin-tsc')
+const fs = require('fs')
 
 const recapDevAutoWrapper = {
   name: 'recap-dev-auto-wrapper',
   setup(build) {
-    build.onLoad({ filter: 'mysql2' }, async (args) => ({
-      contents: `
-        (function() {
-  ${originalSource}
-})(...arguments);
-{
-mod = module.exports;
-const { mysqlQueryWrapper } = require('@recap.dev/client/dist/lib/module-trackers/mysql')
-module.exports = mysqlQueryWrapper(mod, 'mysql2');
+    build.onLoad({ filter: 'mysql2' }, async ({ path }) => {
+      const originalSource = fs.readFileSync(path, 'utf8')
 
-mod.Connection.prototype.query = mysqlQueryWrapper(mod.Connection.prototype.query);
-mod.Connection.prototype.execute = mysqlQueryWrapper(mod.Connection.prototype.execute);
-module.exports = mod;
-`,
-    }))
+      return ({
+        contents: `
+          (function() {
+    ${originalSource}
+  })(...arguments);
+  {
+  mod = module.exports;
+  const { mysqlQueryWrapper } = require('@recap.dev/client/dist/lib/module-trackers/mysql')
+  module.exports = mysqlQueryWrapper(mod, 'mysql2');
+
+  mod.Connection.prototype.query = mysqlQueryWrapper(mod.Connection.prototype.query);
+  mod.Connection.prototype.execute = mysqlQueryWrapper(mod.Connection.prototype.execute);
+  module.exports = mod;
+  `,
+      })
+    })
   },
 }
 
